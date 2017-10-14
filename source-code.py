dd7e3167f2e8c48190e488fc2ff8dc020fc144bb
drubicza <id.slacker@gmail.com>
2017-10-14 20:38:25 +0800

    Refactoring the source code

--- source-code.py
+++ source-code.py
@@ -1,20 +1,13 @@
+#!/bin/env python
 import argparse
 
-halah = argparse.ArgumentParser(description="Filtering words tools by unique")
-halah.add_argument("-i", type=str, action="store", help="Input file name")
-halah.add_argument("-out", type=str, action="store", help="Output file name")
-
+halah = argparse.ArgumentParser(description='Tool for filtering unique words')
+halah.add_argument('-i', type=str, action='store', help='Input file name')
+halah.add_argument('-o', type=str, action='store', help='Output file name')
 zeeb = halah.parse_args()
 
-if zeeb.i and zeeb.out:
-	f = open(zeeb.i, "r").read().split()
-	w = open(zeeb.out, "w")
-	wibu = {""}
-	w.write("Filtered List, Tools can found in https://github.com/B3yeZ/")
-
-	for yo in f:
-		wibu.add(yo)
-	for x in wibu:
-		w.write(x + "\n")
-
-	w.close()
+if zeeb.i and zeeb.o:
+    with open(zeeb.i,'r') as fi, open(zeeb.o,'w') as fo:
+        d = sorted(set(fi.read().split()))
+        fo.write('Filtered list, tools can be found on github: https://github.com/B3yeZ/')
+        fo.write('\n'.join([l for l in d]))
